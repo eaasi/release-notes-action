ae3095d3d307d81c33e706517dff40636d10ce40
Oleg Stobbe <oleg@bitforr.com>
2025-02-24 20:02:40 +0100

    feat: add action's core implementation

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 Yale University
+// SPDX-License-Identifier: Apache-2.0
+
+/** Default release heading prefix */
+export const RELEASE_HEADING_PREFIX = '^## \\[?';
+
+/** Default release heading suffix */
+export const RELEASE_HEADING_SUFFIX = '[\\] ]';
+
+/** Action's input name */
+export enum InputName {
+  CHANGELOG_FILE = 'changelog-file',
+  RELEASE_NOTES_FILE = 'release-notes-file',
+  RELEASE_VERSION = 'release-version',
+};
